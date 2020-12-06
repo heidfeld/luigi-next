49c43692a9d59722ed73482860a2cf643413068c
heidfeld <l.karmanski@gmail.com>
2020-12-06 20:41:37 +0100

    added luigi framework

--- src/pages/views/ekstraklasa.js
+++ src/pages/views/ekstraklasa.js
@@ -1,7 +1,34 @@
+//import Test from '@heidfeld/luigi-framework';
+
 export default function Ekstraklasa(props) {
+
+    const data = {
+        headers: [
+            {id: headerId1, name: 'header 1'},
+            {id: headerId2, name: 'header 2'},
+            {id: headerId3, name: 'header 3'},
+        ],
+        rows: [
+            {
+                [headerId1]: toSimpleDataDTO(1, 'r1'),
+                [headerId2]: toSimpleDataDTO(2, 'r2'),
+                [headerId3]: toSimpleDataDTO(3, 'r3')
+            },
+            {
+                [headerId1]: toSimpleDataDTO('test1', 'test1'),
+                [headerId2]: toSimpleDataDTO('test2', 'test2'),
+                [headerId3]: toSimpleDataDTO('test3', 'test3')
+            },
+            {
+                [headerId1]: toSimpleDataDTO('xd', 'xd'),
+                [headerId2]: toSimpleDataDTO('xh', 'xh'),
+                [headerId3]: toSimpleDataDTO('xc', 'xc')
+            },
+        ]
+    };
+
     return (
         <div>
-            Ekstraklasa
         </div>
     )
 }
