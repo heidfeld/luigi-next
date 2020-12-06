7dcc97cb61c4f82b27d7324aec74373810b621fe
heidfeld <l.karmanski@gmail.com>
2020-12-06 12:03:02 +0100

    ekstraklasa

---
+++ src/pages/views/ekstraklasa.js
@@ -0,0 +1,13 @@
+export default function Ekstraklasa(props) {
+    return (
+        <div>
+            Ekstraklasa
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    return {
+        props: {},
+    }
+}
