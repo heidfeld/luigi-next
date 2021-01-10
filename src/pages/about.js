95075bd5a2f236ad9c68b98c0262d0be75bbff46
heidfeld <l.karmanski@gmail.com>
2021-01-10 14:29:12 +0100

    + changed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,23 @@
+import React from 'react';
+
 import '../styles/less/pages/about.less';
 
+const REACT_JS_LINK = 'http://reactjs.org';
+
 export default function About(props) {
+
     return (
-        <div>
-            About me...
+        <div className='about'>
+            <p>luigi</p>
+            <img className='about__image' src="/leon.png"/>
+            <p>developer</p>
+            <p>
+                <a href={REACT_JS_LINK}>
+                    <span className='about__text--lightskyblue'><b>react</b></span>
+                    .
+                    <span className='about__text--lightcoral'>js</span>
+                </a>
+            </p>
         </div>
     )
 }
